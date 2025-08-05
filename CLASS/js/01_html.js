7423284b01b862d350a7362af3861320693f64a3
svv0003 <ssetoo4689@naver.com>
2025-08-06 02:03:23 +0900

    .

--- CLASS/js/01_html.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  // class="container" 요소에 클릭 이벤트 리스너 추가
-  $(".container").on("click", function () {
-    // 클릭된 컨테이너의 자식 .instruction 요소를 찾아 슬라이드 토글
-    $(this).find(".instruction").slideToggle();
-  });
-});
